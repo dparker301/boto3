60f8166c1f82e9e28947a452bae7ad245872f341
Dewayne Parker <mr.leonard.parker@gmail.com>
2024-07-11 17:05:37 +0000

    Searching for file extension

--- List_objects.py
+++ List_objects.py
@@ -6,13 +6,11 @@ s3 = boto3.client('s3')
 # Listing the Buckets
 response = s3.list_objects_v2(Bucket="dewayne-boto3-06212024")
 
-#print(response)
-
 # Printing the folders in the content  
 for content in response["Contents"]:
     
-    # Adding an if statement since there are multiple files and we just want the text file
-    if(".txt" in content["Key"]):
+    # Adding an if statement since there are multiple files and we just want the text file. Adding the -4 will be the last for characters so you can find the file type
+    if(".txt" in content["Key"][-4:]):
     
         # Printing the content
         print (content["Key"])
